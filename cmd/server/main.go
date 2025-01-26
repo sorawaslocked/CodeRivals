fa882a0ba9550fc7223fa277954cac152f589fff
sora <133684349+sorawaslocked@users.noreply.github.com>
2025-01-26 22:09:59 +0500

    Add simple logging and address command line argument

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+)
 
+func main() {
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	infoLog := log.New(os.Stdin, "INFO\t", log.Ldate|log.Ltime)
+
+	app := application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
+	addr := flag.String("addr", ":8080", "Server port")
+
+	flag.Parse()
+
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: app.errorLog,
+	}
+
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil && errors.Is(err, http.ErrServerClosed) {
+			app.errorLog.Fatal(err)
+		}
+	}()
 }
