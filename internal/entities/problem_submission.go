50858ec40bde862bfbc9ab195c04849edce43b3b
sora <133684349+sorawaslocked@users.noreply.github.com>
2025-01-29 15:13:30 +0500

    Change fields of problem submission

--- internal/entities/problem_submission.go
+++ internal/entities/problem_submission.go
@@ -1,10 +1,14 @@
 package entities
 
+import "time"
+
 type ProblemSubmission struct {
-	UserID    uint    `json:"user_id"`
-	ProblemID uint    `json:"problem_id"`
-	Code      string  `json:"code"`
-	Status    string  `json:"status"`
-	Runtime   int     `json:"runtime_ms"`
-	Error     *string `json:"error"`
+	UserID      uint      `json:"user_id"`
+	ProblemID   uint      `json:"problem_id"`
+	Code        string    `json:"code"`
+	Status      string    `json:"status"`
+	Runtime     uint32    `json:"runtime_ms"`
+	Memory      uint32    `json:"memory_kb"`
+	SubmittedAt time.Time `json:"submitted_at"`
+	Error       string    `json:"error"`
 }
