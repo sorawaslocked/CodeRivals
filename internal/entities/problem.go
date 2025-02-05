7b8f71d9bea24a642b57cac55ec4ab788e86cb99
sora <133684349+sorawaslocked@users.noreply.github.com>
2025-02-05 05:38:46 +0500

    Add the examples slice back to the problem entity

--- internal/entities/problem.go
+++ internal/entities/problem.go
@@ -3,15 +3,16 @@ package entities
 import "time"
 
 type Problem struct {
-	ID          int       `json:"id"`
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	Difficulty  string    `json:"difficulty"`
-	Url         string    `json:"url"`
-	Topics      []*Topic  `json:"topics"`
-	InputTypes  []string  `json:"input_types"`
-	OutputType  string    `json:"output_type"`
-	MethodName  string    `json:"method_name"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	ID          int               `json:"id"`
+	Title       string            `json:"title"`
+	Description string            `json:"description"`
+	Difficulty  string            `json:"difficulty"`
+	Url         string            `json:"url"`
+	Topics      []*Topic          `json:"topics"`
+	InputTypes  []string          `json:"input_types"`
+	OutputType  string            `json:"output_type"`
+	MethodName  string            `json:"method_name"`
+	Examples    []*ProblemExample `json:"examples"`
+	CreatedAt   time.Time         `json:"created_at"`
+	UpdatedAt   time.Time         `json:"updated_at"`
 }
